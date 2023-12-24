fcc4df6199c8b816a07d6f4c5a78d9eaf6e08592
RAMESH ESUKALAPALEPU <102863997+RameshEsukalapalepu@users.noreply.github.com>
2023-12-24 10:35:29 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,21 @@
+let colorPickerContainerElement = document.getElementById("colorPickerContainer");
+            let selectedColorElement = document.getElementById("selectedColor");
+            function changeBgToGreyAndUpdateText(){
+                colorPickerContainerElement.style.backgroundColor="#e0e0e0";
+                selectedColorElement.textContent = "#e0e0e0";
+            }
+            
+            function changeBgToGreenAndUpdateText(){
+                colorPickerContainerElement.style.backgroundColor="#6fcf97";
+                selectedColorElement.textContent = "#6fcf97"
+            }
+            
+            function chageBgToBlueAndUpdateText(){
+                colorPickerContainerElement.style.backgroundColor ="#e0e0e0";
+                selectedColorElement.textContent = "#56ccf2";
+            }
+            
+            function changeBgToPurpleAndUpdateText(){
+                colorPickerContainerElement.style.backgroundColor = "#bb6bd9";
+                selectedColorElement.textContent ="#bb6bd9";
+            }
